69eee54dcd55b5788228c0803d358db520461535
snipershady <snipershady@gmail.com>
2025-12-16 10:39:01 +0000

    Improved test

--- tests/entrypoint.php
+++ tests/entrypoint.php
@@ -5,8 +5,7 @@ header('Content-type:application/json');
 $epti = new TypeIdentifier\Service\EffectivePrimitiveTypeIdentifierService();
 $inputServer = $epti->getTypedValueFromServer('HTTP_USER_AGENT');
 
-$requestMethod = filter_var($_SERVER['REQUEST_METHOD'], FILTER_UNSAFE_RAW);
-
+$requestMethod = $epti->getTypedValueFromServer('REQUEST_METHOD');
 if ('GET' === $requestMethod) {
     $inputParameter = $epti->getTypedValueFromGet('param');
     exit(json_encode(['is_valid' => true, 'value' => $inputParameter, 'agent' => $inputServer]));
